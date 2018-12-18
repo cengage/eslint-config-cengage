a9da64e3fb427853545d7d7a2b15829465f55fb3
Ryan Gillett <ryan.gillett@cengage.com>
2018-12-18 14:50:11 -0500

    adding in new defaults

--- react.js
+++ react.js
@@ -1,18 +1,22 @@
 module.exports = {
   extends: [
-    'cengage/lib/default',
-    'plugin:react/recommended',
-    'plugin:jsx-a11y/recommended'
+    "cengage/lib/default",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended"
   ],
-  plugins: ['react', 'jsx-a11y'],
+  plugins: ["react", "jsx-a11y", "unicorn"],
+  parser: "babel-eslint",
+  env: {
+    jest: true
+  },
   parserOptions: {
     ecmaFeatures: {
       jsx: true
     }
   },
   rules: {
-    'react/boolean-prop-naming': 1,
-    'react/prop-types': 1,
-    'jsx-a11y/heading-has-content': 0
+    "react/boolean-prop-naming": 1,
+    "react/prop-types": 1,
+    "jsx-a11y/heading-has-content": 0
   }
-}
+};
